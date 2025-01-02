1ea0e7bf0a352679c5e5306a4b5b6fcde4b4f4f0
Brajin Hanna <bhanna@somosexperiences.com>
2025-01-02 02:54:55 +0100

    modificando el nombre del errorHandler

--- backoffice/middleware/multer.js
+++ backoffice/middleware/multer.js
@@ -1,35 +1,52 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const utils = require("../utils/utils");
+const MAX_FILE_SIZE = 5;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, '../public/uploads');
+    const uploadPath = path.join(__dirname, "../public/uploads");
     cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}-${file.originalname}`;
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}-${
+      file.originalname
+    }`;
     cb(null, uniqueSuffix);
   },
 });
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: MAX_FILE_SIZE * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
     if (!allowedTypes.includes(file.mimetype)) {
-      return cb(new Error('Only JPEG, PNG, and WEBP are allowed.'));
+      return cb(new Error("Only JPEG, PNG, and WEBP are allowed."));
     }
     cb(null, true);
   },
 });
 
-const multerOnError = (error, req, res, next) => {
-  if (error instanceof multer.MulterError) {
-    res.status(400).json({ error: error.message });
-  } else {
-    res.status(500).json({ error: error.message });
-  }
+const multerErrorHandler = (err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res
+        .status(400)
+        .json(
+          utils.responseError("File size exceeds the limit.", [
+            { msg: `File size exceeds the limit of ${MAX_FILE_SIZE}MB.` },
+          ])
+        );
+    }
+  } else if (err) {
+    return res
+      .status(400)
+      .json(
+        utils.responseError("Failed to process request", [{ msg: err.message }])
+      );
+   }
+  next();
 };
 
-module.exports = {upload, multerOnError};
+module.exports = { upload, multerErrorHandler };
